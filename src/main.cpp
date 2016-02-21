9f8ca0fcf28f67d1cceccddd5d5d88a812a44fce
Husni Munaya <husnimun@gmail.com>
2016-02-21 17:42:16 +0700

    Edit map

--- src/main.cpp
+++ src/main.cpp
@@ -13,15 +13,75 @@ int main() {
 	polygon p1;
 	int coef = 30;
 
-	p1.add_point(2*coef, 0*coef);
-	p1.add_point(4*coef, 2*coef);
-	p1.add_point(4*coef, 4*coef);
-	p1.add_point(7*coef, 7*coef);
-	p1.add_point(6*coef, 8*coef);
-	p1.add_point(5*coef, 8*coef);
-	p1.add_point(2*coef, 5*coef);
-	p1.add_point(2*coef, 0*coef);
+	p1.add_point(2*coef, 1*coef);
+	p1.add_point(4*coef, 3*coef);
+	p1.add_point(4*coef, 5*coef);
+	p1.add_point(7*coef, 8*coef);
+	p1.add_point(6*coef, 9*coef);
+	p1.add_point(5*coef, 9*coef);
+	p1.add_point(2*coef, 6*coef);
+	p1.add_point(2*coef, 1*coef);
+
+	polygon p2;
+	p2.add_point(10*coef, 7*coef);
+	p2.add_point(10*coef, 6*coef);
+	p2.add_point(9*coef, 5*coef);
+	p2.add_point(9*coef, 3*coef);
+	p2.add_point(11*coef, 1*coef);
+	p2.add_point(12*coef, 1*coef);
+	p2.add_point(13*coef, 0*coef);
+	p2.add_point(17*coef, 0*coef);
+	p2.add_point(17*coef, 1*coef);
+	p2.add_point(15*coef, 3*coef);
+	p2.add_point(17*coef, 5*coef);
+	p2.add_point(15*coef, 7*coef);
+	p2.add_point(10*coef, 7*coef);
+
+	polygon p3;
+	p3.add_point(9*coef, 9*coef);  
+	p3.add_point(15*coef, 9*coef);  
+	p3.add_point(17*coef, 11*coef);  
+	p3.add_point(10*coef, 11*coef);  
+	p3.add_point(9*coef, 10*coef);  
+	p3.add_point(9*coef, 9*coef);
+
+	polygon p4;
+	p4.add_point(20*coef, 7*coef);
+	p4.add_point(20*coef, 3*coef);
+	p4.add_point(22*coef, 1*coef);
+	p4.add_point(26*coef, 1*coef);
+	p4.add_point(26*coef, 2*coef);
+	p4.add_point(25*coef, 3*coef);
+	p4.add_point(24*coef, 3*coef);
+	p4.add_point(23*coef, 4*coef);
+	p4.add_point(25*coef, 6*coef);
+	p4.add_point(25*coef, 7*coef);
+	p4.add_point(26*coef, 8*coef);
+	p4.add_point(26*coef, 9*coef);
+	p4.add_point(24*coef, 9*coef);
+	p4.add_point(22*coef, 7*coef);
+	p4.add_point(20*coef, 9*coef);
+
 	
+	polygon p6;
+	p6.add_point(37*coef, 10*coef);
+	p6.add_point(36*coef, 9*coef);
+	p6.add_point(35*coef, 9*coef);
+	p6.add_point(32*coef, 6*coef);
+	p6.add_point(31*coef, 6*coef);
+	p6.add_point(30*coef, 5*coef);
+	p6.add_point(28*coef, 3*coef);
+	p6.add_point(28*coef, 2*coef);
+	p6.add_point(31*coef, 2*coef);
+	p6.add_point(33*coef, 4*coef);
+	p6.add_point(36*coef, 1*coef);
+	p6.add_point(40*coef, 1*coef);
+	p6.add_point(40*coef, 10*coef);
+	p6.add_point(37*coef, 10*coef);
+
+	
+
+
 	
 
 	int width = 480;
@@ -41,9 +101,16 @@ int main() {
 
 	while (true) {
         v.draw(p1);
+        v.draw(p2);
+        v.draw(p3);
+        v.draw(p4);
+        v.draw(p6);
 		p1.draw_stroke(0, 0);
+		p2.draw_stroke(0, 0);
+		p3.draw_stroke(0, 0);
+		p4.draw_stroke(0, 0);
+		p6.draw_stroke(0,0);
 		canvas::get_instance()->render();
-		canvas::get_instance()->clear();
         c = getch();   
         switch(c) {
             case 122:       // key z
@@ -53,21 +120,22 @@ int main() {
                 v.zoom(1);
                 break;
             case 65:       // key up
-                v.move_clip(0, -1);
+                v.move_clip(0, -4);
                 break;
             case 66:       // key down
-                v.move_clip(0, 1);     
+                v.move_clip(0, 4);     
                 break;
             case 67:       // key right
-                v.move_clip(1, 0);
+                v.move_clip(4, 0);
                 break;
             case 68:       // key left
-                v.move_clip(-1, 0);
+                v.move_clip(-4, 0);
                 break;
             default:
                 v.move_clip(0, 0);
                 break;
         }
+		canvas::get_instance()->clear();
 		//v.move_clip(1, 0);
 		//v.draw(p1);	
 
